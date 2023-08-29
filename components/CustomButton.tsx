0bbec3252aca2146fb28a70285e751bf6a9b715d
Aranza <aranzaiortegas@gmail.com>
2023-08-29 18:29:26 +0200

    add layout and assets

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Image from 'next/image'
+import { CustomButtonProps } from '@/types'
 
-const CustomButton = () => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
   )
 }
 
