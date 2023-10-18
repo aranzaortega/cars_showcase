8001bc5cc8f4c3053f2efaff93463b6d4a8aff03
Aranza <aranzaiortegas@gmail.com>
2023-10-18 12:47:47 +0200

    comment navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className='relative'>
-        <NavBar />
+        {/* <NavBar /> */}
         {children}
         <Footer />
       </body>
--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,6 @@
 import Hero from './Hero';
 import CustomButton from './CustomButton';
-import NavBar from './NavBar';
+// import NavBar from './NavBar';
 import Footer from './Footer';
 import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
@@ -12,7 +12,7 @@ import ShowMore from './ShowMore';
 export {
     Hero,
     CustomButton,
-    NavBar,
+    // NavBar,
     Footer,
     SearchBar,
     CustomFilter,
