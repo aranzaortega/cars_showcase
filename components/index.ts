15b3d3eeceef5bd3605ee1447493ffd0bffedbe2
Aranza <aranzaiortegas@gmail.com>
2023-10-18 12:42:11 +0200

    change index components

--- components/index.ts
+++ components/index.ts
@@ -1,13 +1,13 @@
-import Hero from './Hero'
-import CustomButton from './CustomButton'
-import NavBar from './NavBar'
-import Footer from './Footer'
-import SearchBar from './SearchBar'
-import CustomFilter from './CustomFilter'
-import SearchManufacturer from './SearchManufacturer'
-import CarCard from './CarCard'
-import CardDetails from './CardDetails'
-import ShowMore from './ShowMore'
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import NavBar from './NavBar';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CardDetails from './CardDetails';
+import ShowMore from './ShowMore';
 
 export {
     Hero,
@@ -19,5 +19,5 @@ export {
     SearchManufacturer,
     CarCard,
     CardDetails,
-    ShowMore
+    ShowMore,
 }
